c1bda40ae59f04e1dc42f6c570bb410dca2a22bc
Cokaine2912 <niraj28352@gmail.com>
2024-01-27 19:12:39 +0530

    Task_Eight_Fixed

--- NodeJS/parsing_req.js
+++ NodeJS/parsing_req.js
@@ -10,18 +10,17 @@ function rqListener(req, res) {
     let content;
     fs.readFile(fpath, "utf8", (err, data) => {
       content = data;
-    });
-    console.log("content", content);
+      //console.log("content", content);
 
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      `<body><form action="/message" method="POST">${content}<br><br><input type="text" name="message"><button type="submit">Send</button></form></body>`
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(
+        `<body><form action="/message" method="POST">${content}<br><br><input type="text" name="message"><button type="submit">Send</button></form></body>`
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
       console.log(chunk);
@@ -31,18 +30,18 @@ function rqListener(req, res) {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(`<body><h1>Hello From my Node.js Server</h1></body>`);
+    res.write("</html>");
+    res.end();
   }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write(`<body><h1>Hello From my Node.js Server</h1></body>`);
-  res.write("</html>");
-  res.end();
 }
 
 const server = http.createServer(rqListener);
